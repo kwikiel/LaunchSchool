7bc8db52940b1c20797cdf70de731101359e75bd
Kacper Wikieł <kwikiel@gmail.com>
2021-06-09 12:11:22 +0200

    feat: calculate cat age in human years

---
+++ JS100/WorkingCode/TestCalculateCatAge.js
@@ -0,0 +1,9 @@
+var main = require('CalculateCatAge.js')
+
+
+function testCatAge(){
+    if(CalculateCatAge.calculate(1+1)==2)
+       console.log('It Works!');
+    else
+       console.log('Test failed');
+}
