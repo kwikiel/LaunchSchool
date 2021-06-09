c485b1079adda55cb4fa345d7a8d6cf34880f143
Kacper Wikieł <kwikiel@gmail.com>
2021-06-09 09:59:03 +0200

    refactor: make sentence arrow function

---
+++ JS100/WorkingCode/ArrowFunctions.js
@@ -0,0 +1,13 @@
+const template = 'I VERB NOUN.';
+
+/*
+function sentence(verb, noun) {
+  return template
+    .replace('VERB', verb)
+    .replace('NOUN', noun);
+}
+*/
+
+let sentence = (verb, noun) => template.replace('VERB', verb).replace('NOUN', noun)
+
+console.log(sentence('like', 'birds'));
