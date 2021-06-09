6e441578e368d9e635fd149c90a71754bd7d0a11
Kacper Wikieł <k.wikiel@gmail.com>
2021-06-09 16:18:33 +0200

    learn: addign a property with a whitespace

--- JS100/WorkingCode/RetrieveValue1.js
+++ JS100/WorkingCode/RetrieveValue1.js
@@ -19,7 +19,23 @@ let jane = {
   }; 
 
 console.log(student.courses);
-
 console.log(student.locker); // undefined 
-
 console.log(jane.location.country)
+
+let fido = {
+    name: 'Fido',
+    species: 'Labrador Retriever',
+    color: 'brown',
+    weight: 16,
+  };
+
+
+// Add property 'age'.
+// Add property 'favorite food'.
+fido.age = 21; 
+fido["favourite food"] = "pizza"; 
+
+console.log("Add a property"); 
+
+console.log(fido.age);
+console.log(fido.favourite_food);
