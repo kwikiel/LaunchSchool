02a825e9426c2fb5854d9159d120c476b93daac4
Kacper Wikieł <k.wikiel@gmail.com>
2021-06-22 21:59:41 +0200

    chore: eslint

--- JS101/Easy1/HowBigIsTheRoom.js
+++ JS101/Easy1/HowBigIsTheRoom.js
@@ -1,9 +1,9 @@
-let readlineSync = require("readline-sync");
+const readlineSync = require('readline-sync');
 
-var length = readlineSync.question("Enter the length of the room in meters:");
-var width = readlineSync.question("Enter the length of the room in meters:");
+const length = readlineSync.question('Enter the length of the room in meters:');
+const width = readlineSync.question('Enter the length of the room in meters:');
 
-let area = parseInt(length) * parseInt(width);
-const M2TOFEET2 = 10.76; 
+const area = parseInt(length) * parseInt(width);
+const M2TOFEET2 = 10.76;
 
-console.log(`The area of the room is ${area} square meters (${area*m2tofeet2} square feet)`)
+console.log(`The area of the room is ${area} square meters (${area*M2TOFEET2} square feet)`);
