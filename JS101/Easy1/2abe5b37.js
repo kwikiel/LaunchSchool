84eaa0702e88e9cbda200384d6e13113d2a412a8
Kacper Wikieł <k.wikiel@gmail.com>
2021-06-22 21:57:29 +0200

    chore: linter first fix try

--- JS101/Easy1/2abe5b37.js
+++ JS101/Easy1/2abe5b37.js
@@ -1,18 +1,16 @@
 // Goal to write a function that takes one integer
-// Argument may be positive, negative or zero 
-// Return true if the value is odd 
+// Argument may be positive, negative or zero
+// Return true if the value is odd
 // One may assume that argument is a valid integer
 
-const isOdd  = function(num){
-    if(Math.abs(num) % 2 === 1){
-        return true;
-    }
-    return false;
-}
+const isOdd = function(num) {
+  if (Math.abs(num) % 2 === 1) {
+    return true;
+  }
+  return false;
+};
 
-// Test harness 
-
-
+// Test harness
 console.log(isOdd(2)); // => false
 console.log(isOdd(5)); // => true
 console.log(isOdd(-17)); // => true
