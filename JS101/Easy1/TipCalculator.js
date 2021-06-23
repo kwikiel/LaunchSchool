8ad51cb6023fe4fa284bb881e0c6fecdd8d7a665
Kacper Wikieł <k.wikiel@gmail.com>
2021-06-23 08:58:42 +0200

    chore: linter

--- JS101/Easy1/TipCalculator.js
+++ JS101/Easy1/TipCalculator.js
@@ -1,15 +1,16 @@
-// Required dependency 
-const readlineSync = require("readline-sync");
+// Required dependency
+const readlineSync = require('readline-sync');
 
 // How to use readline-sync
-// var length = readlineSync.question("Enter the length of the room in meters:");
+// var length =
+// readlineSync.question("Enter the length of the room in meters:");
 
-// Exercise 
-//What is the bill? 200
-//What is the tip percentage? 15
+// Exercise
+// What is the bill? 200
+// What is the tip percentage? 15
 
-//The tip is $30.00
-//The total is $230.00
+// The tip is $30.00
+// The total is $230.00
 
 const bill = readlineSync.question('What is the bill? ');
 const percentage = readlineSync.question('What is the tip percentage? ');
