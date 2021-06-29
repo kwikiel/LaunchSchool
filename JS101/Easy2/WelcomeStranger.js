c768aa6b93930997d92fdf6978e4620f7693a4b6
Kacper Wikiel <k.wikiel@gmail.com>
2021-06-29 15:41:39 +0200

    pedac: outline welcome stranger function with PEDAC process

---
+++ JS101/Easy2/WelcomeStranger.js
@@ -0,0 +1,29 @@
+// PEDAC 
+// Problem
+// Example 
+// Data structure 
+// Algo 
+// Implementation quirks
+
+// ## Problem: Create a function that takes 2 arguments: array and tan object. 
+// Array will contain 2 or more elements that, when combined with adjoining spaces, will produce person's name. 
+// The Object will contain two keys: "title" and "occupation"
+// Function should return a greeting that uses the person's full name, and mentions the person's title 
+
+// ## Example 
+// 
+//console.log(
+//    greetings(["John", "Q", "Doe"], { title: "Master", occupation: "Plumber" })
+//  );
+// logs Hello, John Q Doe! Nice to have a Master Plumber around.
+
+// ## Data structure 
+// function ([Array], [Object]) => [String]
+
+// ## Algorithm 
+// Concatenate array elements 
+// Add "Nice to have a" string 
+// Access object key "title"
+// Access object key "occupation"
+
+// Using string formatting `${}` return outcome 
